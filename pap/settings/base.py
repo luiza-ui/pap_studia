eb00b66072a30099cd0f5cd798df623f90a4ae47
luiza-ui <luizaalves2520@gmail.com>
2026-07-07 03:03:28 +0100

    Update base.py

--- pap/settings/base.py
+++ pap/settings/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'anymail',
     'apps.accounts',
     'apps.resources',
     'apps.favorites',
