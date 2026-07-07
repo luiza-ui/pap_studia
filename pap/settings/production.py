df69f57b20f8bacecf017abfaf81dc2e10db9e54
luiza-ui <luizaalves2520@gmail.com>
2026-07-07 10:41:28 +0100

    Update production.py

--- pap/settings/production.py
+++ pap/settings/production.py
@@ -41,6 +41,7 @@ EMAIL_BACKEND    = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST       = 'smtp.gmail.com'
 EMAIL_PORT       = 587
 EMAIL_USE_TLS    = True
+EMAIL_TIMEOUT = 5
 EMAIL_HOST_USER  = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL  = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@studia.pt')
